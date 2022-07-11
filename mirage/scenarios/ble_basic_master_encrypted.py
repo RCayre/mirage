ee4ff218fc24f8edfeb04d828d2b40c29784dee1
RCayre <rcayre@laas.fr>
2022-07-11 14:17:26 +0200

    Adding example scenario of basic master encryption

---
+++ mirage/scenarios/ble_basic_master_encrypted.py
@@ -0,0 +1,17 @@
+from mirage.core import scenario
+from mirage.libs import io,ble,bt,utils,wireless
+from mirage.libs.common import parsers
+import random
+
+class ble_basic_master_encrypted(scenario.Scenario):
+	def onStart(self):
+		self.emitter = self.module.emitter
+		self.receiver = self.module.receiver
+		self.module.connect()
+		return True
+		
+	def onSlaveConnect(self):
+		self.module.discover()
+		if self.emitter.encryptLink(rand=bytes.fromhex(self.args["RAND"]), ediv=utils.integerArg(self.args["EDIV"]), ltk = bytes.fromhex(self.args["LTK"])[::-1]):
+			io.success("Encryption successfully enabled =)")
+			self.module.discover()
